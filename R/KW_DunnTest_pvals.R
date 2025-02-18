acf64df78b6b57d1cec54d3a916a2226bf28d32d
NeilDube <114532934+neildube@users.noreply.github.com>
2025-02-18 13:47:03 +0100

    Add files via upload

--- R/KW_DunnTest_pvals.R
+++ R/KW_DunnTest_pvals.R
@@ -1,16 +1,21 @@
 #' StatTest and Posthoc P values of multiple interdependent variables in a
 #' dataframe (based on Rstatix)
-#' @param x
-#' @param VC1_col
-#' @param VC2_col
-#' @param p.adj.method
+#' @param x - Your dataset
+#' @param VC1_col - first grouping variable
+#' @param VC2_col - second grouping variable (This is used as the factor in the test, column ~ VC2_col)
+#' @param p.adj.method - method to adjust p values for multiple comparisons. Used when pairwise comparisons are performed. Allowed values include "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none". If you don't want to adjust the p value (not recommended), use p.adjust.method = "none".
+#' @import dplyr
+#' @import reshape2
+#' @import rstatix
+#' @import tidyverse
 #' @description
 #' Runs sttistical significance tests and its associated Posthoc and gives the
 #' results in a dataframe. To use the package and the associated command add the
 #'  write the command and add the data neccessary data set.The data set should
 #'  have two variable/grouping columns, eg: Treatment and Sampling Day. Add your
 #'  grouping variable to VC1_col eg: VC1_col = "Day" and variable to VC2_col
-#'  eg: VC2_col = "Treatment". The package currently only has one command but I
+#'  eg: VC2_col = "Treatment" and what correction you want eg, holm, hummel,
+#'  bonferr.. etc. . The package currently only has one command but I
 #'  will update soon (If you need something urgent please write to me on the
 #'  provided email)
 #'
@@ -26,7 +31,7 @@ KW_DunnTest_pvals<- function(x, VC1_col, VC2_col, p.adj.method) {
   ym <- data.frame(reshape2::melt(y, na.rm = TRUE))
 
   Kruskal_result <- ym %>% group_by(c2, variable) %>% rstatix::kruskal_test(value ~
-                                                                         c3)
+                                                                              c3)
   Kruskal_result2 <- cbind(
     Kruskal_result[,8],
     Kruskal_result[,1],
@@ -43,9 +48,12 @@ KW_DunnTest_pvals<- function(x, VC1_col, VC2_col, p.adj.method) {
       value.var = "p"
     )
   )
+  P_ADJ_Method<-c("")
+
+  Kruskal_result4<-cbind(Kruskal_result3, P_ADJ_Method)
 
   Dunntest_result <- ym %>% group_by(c2, variable) %>% rstatix::dunn_test(value ~
-                                                                   c3, p.adjust.method = p.adj.method)
+                                                                            c3, p.adjust.method = p.adj.method)
   Dunntest_result <- Dunntest_result %>% mutate(Group = paste(group1, "-", group2))
   Dunntest_result2 <- cbind(
     Dunntest_result[,1],
@@ -64,10 +72,13 @@ KW_DunnTest_pvals<- function(x, VC1_col, VC2_col, p.adj.method) {
       value.var = "p.adj"
     )
   )
+  P_ADJ_Method<-c(p.adj.method)
 
-  Kruskal_result3<-setNames(Kruskal_result3, replace(names(Kruskal_result3), 1:3, c("Test", VC1_col, "Formula/Group")))
-  Dunntest_result3<-setNames(Dunntest_result3, replace(names(Dunntest_result3), 1:3, c("Test", VC1_col, "Formula/Group")))
+  Dunntest_result4<-cbind(Dunntest_result3, P_ADJ_Method)
+
+  Kruskal_result4<-stats::setNames(Kruskal_result4, replace(names(Kruskal_result4), 1:3, c("Test", VC1_col, "Formula/Group")))
+  Dunntest_result4<-stats::setNames(Dunntest_result4, replace(names(Dunntest_result4), 1:3, c("Test", VC1_col, "Formula/Group")))
 
 
-  return(rbind(Kruskal_result3, Dunntest_result3))
+  return(rbind(Kruskal_result4, Dunntest_result4))
 }
